6e44060006b187d53dbbf16b02c57bcc022a4a6d
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-10-27 15:00:53 -0300

    fix: springbootapp

--- src/main/java/com/school/studentsrecordsdb/StudentsrecordsdbApplication.java
+++ src/main/java/com/school/studentsrecordsdb/StudentsrecordsdbApplication.java
@@ -2,9 +2,8 @@ package com.school.studentsrecordsdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class StudentsrecordsdbApplication {
 
 	public static void main(String[] args) {
