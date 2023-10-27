5f29e89e528d242135c4ae93308d286eaee9113a
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-10-27 00:57:12 -0300

    feature: post-student

--- src/main/java/com/school/studentsrecordsdb/controller/StudentController.java
+++ src/main/java/com/school/studentsrecordsdb/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.school.studentsrecordsdb.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +26,13 @@ public class StudentController {
         return studentRepository.findAll();
     }
 
+    @GetMapping("/student/{id}")
+    public Optional<Student> getStudentById(@PathVariable(value = "id") Integer id){
+        return studentRepository.findById(id);
+    }
+
+    @PostMapping("/student")
+    public Student createStudent(@RequestBody Student student){
+        return studentRepository.save(student);
+    }
 }
--- src/main/java/com/school/studentsrecordsdb/repository/StudentRepository.java
+++ src/main/java/com/school/studentsrecordsdb/repository/StudentRepository.java
@@ -1,9 +1,13 @@
 package com.school.studentsrecordsdb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.school.studentsrecordsdb.models.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer>{
     
+    Optional<Student> findById(Integer id);
+
 }
