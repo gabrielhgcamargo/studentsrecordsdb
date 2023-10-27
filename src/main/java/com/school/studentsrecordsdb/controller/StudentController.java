a0f8ce143b714039360eab1bc9840ed36af1c6a3
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-10-27 13:31:13 -0300

    feature: swagger config

--- src/main/java/com/school/studentsrecordsdb/controller/StudentController.java
+++ src/main/java/com/school/studentsrecordsdb/controller/StudentController.java
@@ -1,10 +1,10 @@
 package com.school.studentsrecordsdb.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,31 +19,40 @@ import org.springframework.web.server.ResponseStatusException;
 import com.school.studentsrecordsdb.models.Student;
 import com.school.studentsrecordsdb.repository.StudentRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value = "/school")
+@Api(value = "API REST -> Students")
+@CrossOrigin(origins = "*")
 public class StudentController {
     
     @Autowired
     StudentRepository studentRepository;
 
     @GetMapping("/students")
+    @ApiOperation(value = "List all students.")
     public List<Student> findAllStudents(){
         return studentRepository.findAll();
     }
 
     @GetMapping("/student/{id}")
+    @ApiOperation(value = "Find a student by ID.")
     public Student getStudentById(@PathVariable(value = "id") Integer id){
         return studentRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
         "Student not found with this ID."));
     }
 
     @PostMapping("/student")
+    @ApiOperation(value = "Create a student.")
     @ResponseStatus(HttpStatus.CREATED)
     public Student createStudent(@RequestBody Student student){
         return studentRepository.save(student);
     }
 
     @DeleteMapping("/student/{id}")
+    @ApiOperation(value = "Delete a Student by ID.")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteStudentById(@PathVariable(value = "id") Integer id){
          studentRepository.findById(id).map(student -> {
@@ -55,6 +64,7 @@ public class StudentController {
 
     
     @PutMapping("/student/{id}")
+    @ApiOperation(value = "Update a Student by ID.")
     public Student updateStudentById(@PathVariable(value = "id") Integer id, @RequestBody Student student){
          return studentRepository.findById(id).map(student1 -> {
             student.setId(student1.getId());
--- src/main/java/com/school/studentsrecordsdb/repository/StudentRepository.java
+++ src/main/java/com/school/studentsrecordsdb/repository/StudentRepository.java
@@ -1,9 +1,7 @@
 package com.school.studentsrecordsdb.repository;
 
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.school.studentsrecordsdb.models.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Integer>{
