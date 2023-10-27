e1b121f431bf1e1b7d4519ff4b2051266916bf53
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-10-27 14:52:15 -0300

    fix: springbootapp - datasourceconfig exclude

--- src/main/java/com/school/studentsrecordsdb/StudentsrecordsdbApplication.java
+++ src/main/java/com/school/studentsrecordsdb/StudentsrecordsdbApplication.java
@@ -2,8 +2,9 @@ package com.school.studentsrecordsdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class StudentsrecordsdbApplication {
 
 	public static void main(String[] args) {
